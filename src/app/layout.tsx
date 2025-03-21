28a1a1316fa0adfa2dd828f858ae8b840d602611
Marco <serikbaev03beka@gmail.com>
2025-03-21 18:14:32 +0500

    refactor(pages): change files direction

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,10 +1,11 @@
 import type { Metadata } from "next";
-import "./globals.css";
+import "./(root)/globals.css";
 
 export const metadata: Metadata = {
   title: "LDA Voice | Смотри аниме онлайн с озвучкой",
   description:
     "Смотри любимые аниме-сериалы онлайн в высоком качестве с профессиональной озвучкой. Удобный выбор серий, сезонов и интерфейс!",
+  icons: "./favicon.ico",
 };
 
 export default function RootLayout({
