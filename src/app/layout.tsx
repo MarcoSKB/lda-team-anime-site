045eb5d669ad504a60437958d9349258dea1c952
Marco <serikbaev03beka@gmail.com>
2025-03-21 23:30:33 +0500

    refactor(files): Change code with prettier

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,21 +1,21 @@
-import type { Metadata } from "next";
-import "./(root)/globals.css";
+import type { Metadata } from 'next'
+import './(root)/globals.css'
 
 export const metadata: Metadata = {
-  title: "LDA Voice | Смотри аниме онлайн с озвучкой",
+  title: 'LDA Voice | Смотри аниме онлайн с озвучкой',
   description:
-    "Смотри любимые аниме-сериалы онлайн в высоком качестве с профессиональной озвучкой. Удобный выбор серий, сезонов и интерфейс!",
-  icons: "./favicon.ico",
-};
+    'Смотри любимые аниме-сериалы онлайн в высоком качестве с профессиональной озвучкой. Удобный выбор серий, сезонов и интерфейс!',
+  icons: './favicon.ico',
+}
 
 export default function RootLayout({
   children,
 }: Readonly<{
-  children: React.ReactNode;
+  children: React.ReactNode
 }>) {
   return (
-    <html lang='ru'>
+    <html lang="ru">
       <body className={`antialiased`}>{children}</body>
     </html>
-  );
+  )
 }
