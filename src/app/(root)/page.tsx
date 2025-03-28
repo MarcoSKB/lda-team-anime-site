02c8cdcf592ec03e9504e686e5308325e894292d
Marco <serikbaev03beka@gmail.com>
2025-03-28 10:23:06 +0500

    refactor(home-page): remove link components

--- src/app/(root)/page.tsx
+++ src/app/(root)/page.tsx
@@ -1,11 +1,5 @@
-import Link from 'next/link'
-
-export default function Home() {
-  return (
-    <div className='flex gap-2 text-lg'>
-      <Link href='/catalog'>Каталог</Link>
-      <Link href='/schedule'>Расписания</Link>
-      <Link href='/voiceover'>Заказать озвучку</Link>
-    </div>
-  )
+const page: React.FC = () => {
+  return <div>Home page</div>
 }
+
+export default page
