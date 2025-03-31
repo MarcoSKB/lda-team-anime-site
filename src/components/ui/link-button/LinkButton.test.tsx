b93a34c96d920d3514104d178fa7b5a2371791f1
Marco <serikbaev03beka@gmail.com>
2025-03-31 22:55:09 +0500

    test(component): add test to LinkButton component

--- src/components/ui/link-button/LinkButton.test.tsx
+++ src/components/ui/link-button/LinkButton.test.tsx
@@ -1,28 +1,20 @@
-import { fireEvent, render, screen } from '@testing-library/react'
+import { render, screen } from '@testing-library/react'
 
 import LinkButton from './LinkButton'
 
-describe('Button Component', () => {
-  it('has type property', () => {
-    render(<LinkButton href='/catalog'>Test</LinkButton>)
-    expect(screen.getByText('Test')).toHaveProperty('type')
+describe('LinkButton Component', () => {
+  it('correctly render text content', () => {
+    render(<LinkButton href='/catalog'>Link text</LinkButton>)
+    expect(screen.getByText('Link text')).toHaveTextContent('Link text')
   })
 
   it('renders correctly', () => {
-    render(<LinkButton href='/catalog'>Click Me</LinkButton>)
-    expect(screen.getByText('Click Me')).toBeInTheDocument()
+    render(<LinkButton href='/catalog'>Link text</LinkButton>)
+    expect(screen.getByText('Link text')).toBeInTheDocument()
   })
 
-  //   it('applies className correctly', () => {
-  //     render(<LinkButton className='custom-class'>Test</LinkButton>)
-  //     expect(screen.getByText('Test')).toHaveClass('custom-class')
-  //   })
-
-  //   it('handles click event', () => {
-  //     const handleClick = vi.fn()
-  //     render(<LinkButton onClick={handleClick}>Click</LinkButton>)
-
-  //     fireEvent.click(screen.getByText('Click'))
-  //     expect(handleClick).toHaveBeenCalledTimes(1)
-  //   })
+  it('has href property', () => {
+    render(<LinkButton href='/catalog'>Link text</LinkButton>)
+    expect(screen.getByText('Link text')).toHaveAttribute('href')
+  })
 })
