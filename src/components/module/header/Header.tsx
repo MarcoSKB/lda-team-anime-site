a7ad8b9b537a00c78dfdb37608a38239a932941d
Marco <serikbaev03beka@gmail.com>
2025-04-05 15:30:05 +0500

    style(header): add adaptive to mobile screen

--- src/components/module/header/Header.tsx
+++ src/components/module/header/Header.tsx
@@ -3,22 +3,19 @@ import Link from 'next/link'
 import { Search } from '@/components/module'
 import { Container } from '@/components/ui'
 
-import Logotype from './components/Logotype'
-import Menu from './components/Menu'
-import Navigation from './components/Navigation'
-import Profile from './components/Profile'
+import { Logotype, Menu, Navigation, Profile } from './components'
 
 const Header: React.FC = () => {
   return (
-    <header>
-      <Container className='bg-secondary absolute bottom-0 left-0 z-10 flex items-center justify-evenly gap-2.5 overflow-hidden py-1.5 md:static md:justify-between md:gap-2 md:bg-transparent md:py-3'>
+    <header className='absolute min-h-screen md:static md:min-h-auto'>
+      <Container className='shadow-top border-secondary bg-background fixed bottom-1 left-0 z-50 ml-[1%] flex max-w-[98%] items-center justify-evenly gap-2.5 overflow-hidden rounded-2xl border-1 border-solid md:static md:mx-auto md:w-auto md:max-w-[1320px] md:justify-between md:gap-2 md:border-none md:bg-transparent md:py-3'>
         <Logotype />
         <Navigation />
         <div className='order-3 flex flex-1 items-center md:order-none md:flex-none md:gap-2'>
           <Search />
           <Link
             href='/donate'
-            className='hover:bg-accent hidden rounded-md bg-[rgba(255,255,255,10%)] px-3 py-2 text-[12px] transition-all ease-out md:flex'
+            className='hover:bg-accent text-foreground hidden rounded-md bg-[rgba(255,255,255,10%)] px-3 py-2 text-[12px] transition-all ease-out hover:text-white md:flex'
           >
             Поддержать проект
           </Link>
---
+++ src/components/module/header/components/index.ts
@@ -0,0 +1,4 @@
+export { default as Logotype } from './Logotype'
+export { default as Menu } from './Menu'
+export { default as Navigation } from './Navigation'
+export { default as Profile } from './Profile'
