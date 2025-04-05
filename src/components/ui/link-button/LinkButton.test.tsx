14206bfb45ebaa2119a600726cc062e1d4a6d40b
Marco <serikbaev03beka@gmail.com>
2025-04-05 15:31:21 +0500

    refactor(test): remove comments in LinkButton test file

--- src/components/ui/link-button/LinkButton.test.tsx
+++ src/components/ui/link-button/LinkButton.test.tsx
@@ -12,17 +12,4 @@ describe('Button Component', () => {
     render(<LinkButton href='/catalog'>Click Me</LinkButton>)
     expect(screen.getByText('Click Me')).toBeInTheDocument()
   })
-
-  //   it('applies className correctly', () => {
-  //     render(<LinkButton className='custom-class'>Test</LinkButton>)
-  //     expect(screen.getByText('Test')).toHaveClass('custom-class')
-  //   })
-
-  //   it('handles click event', () => {
-  //     const handleClick = vi.fn()
-  //     render(<LinkButton onClick={handleClick}>Click</LinkButton>)
-
-  //     fireEvent.click(screen.getByText('Click'))
-  //     expect(handleClick).toHaveBeenCalledTimes(1)
-  //   })
 })
