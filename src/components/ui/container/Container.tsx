40bb578cf237abf5172343d3e251a07c5e8677a5
Marco <serikbaev03beka@gmail.com>
2025-04-08 18:17:06 +0500

    feat(component): add div props to container component

--- src/components/ui/container/Container.tsx
+++ src/components/ui/container/Container.tsx
@@ -2,17 +2,18 @@ import { cva } from 'class-variance-authority'
 
 import { cn } from '@/utils/cn'
 
-interface Props {
+interface Props extends React.ComponentProps<'div'> {
   className?: string
   children: React.ReactNode
 }
 
 const Container: React.FC<Props> = (props) => {
-  const { className, children } = props
+  const { className, children, ...otherProps } = props
 
   return (
     <div
       className={cn('mx-auto box-border w-full max-w-[1320px] px-4', className)}
+      {...otherProps}
     >
       {children}
     </div>
