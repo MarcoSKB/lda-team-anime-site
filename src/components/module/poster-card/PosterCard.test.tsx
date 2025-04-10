28b7917a4a66690c926d697a57b1ddf4366fd2e4
Marco <serikbaev03beka@gmail.com>
2025-04-11 01:19:14 +0500

    test(component): add unit test to PosterCard

---
+++ src/components/module/poster-card/PosterCard.test.tsx
@@ -0,0 +1,31 @@
+import userEvent from '@testing-library/dom'
+import { render, screen } from '@testing-library/react'
+
+import PosterCard from './PosterCard'
+
+const mockData = {
+  id: 0,
+  slug: 'slug-test',
+  title: 'Название постер аниме',
+  img: '/images/placeholder-image.jpg',
+  tags: ['Тест'],
+  rating: 10,
+  description: 'Описание',
+}
+
+describe('Button Component', () => {
+  it('renders correctly', () => {
+    render(<PosterCard {...mockData} />)
+    expect(screen.getByText('Название постер аниме')).toBeInTheDocument()
+  })
+
+  it('has hover class', () => {
+    render(<PosterCard {...mockData} />)
+
+    const cardInfo = screen.getByText('Название постер аниме').parentElement
+    const card = cardInfo?.parentElement
+
+    expect(cardInfo).toHaveClass('group-hover:opacity-100')
+    expect(card).toHaveClass('group')
+  })
+})
