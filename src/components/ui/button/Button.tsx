337cd304933e15c46472d01f229145f697d75d3c
Marco <serikbaev03beka@gmail.com>
2025-04-17 10:57:28 +0500

    fix(style): remove focus styles from button component

--- src/components/ui/button/Button.tsx
+++ src/components/ui/button/Button.tsx
@@ -6,36 +6,33 @@ import { VariantProps, cva } from 'class-variance-authority'
 
 import { cn } from '@/utils/cn'
 
-const buttonStyle = cva(
-  'flex cursor-pointer active:hover:scale-90 focus:outline-none focus-visible:outline-auto ease-out',
-  {
-    variants: {
-      intent: {
-        default:
-          'rounded-md bg-transparent px-2 py-2 transition-colors hover:bg-secondary',
-        primary:
-          'hover:bg-accent rounded-md bg-secondary text-foreground transition-all disabled:pointer-events-none disabled:hover:bg-secondary',
-        secondary:
-          'hover:bg-foreground hover:text-background rounded-md bg-secondary text-foreground transition-all',
-        outline:
-          "hover:bg-foreground hover:text-background border-foreground border-1 border-solid rounded-md bg-secondary text-foreground transition-all'",
-      },
-      size: {
-        small: 'text-[12px] px-2 py-2',
-        default: 'text-sm px-3 py-2',
-      },
-      withIcon: {
-        true: 'gap-2 items-center',
-        false: '',
-      },
+const buttonStyle = cva('flex cursor-pointer active:hover:scale-90  ease-out', {
+  variants: {
+    intent: {
+      default:
+        'rounded-md bg-transparent px-2 py-2 transition-colors hover:bg-secondary',
+      primary:
+        'hover:bg-accent rounded-md bg-secondary text-foreground transition-all disabled:pointer-events-none disabled:hover:bg-secondary',
+      secondary:
+        'hover:bg-foreground hover:text-background rounded-md bg-secondary text-foreground transition-all',
+      outline:
+        "hover:bg-foreground hover:text-background border-foreground border-1 border-solid rounded-md bg-secondary text-foreground transition-all'",
     },
-    defaultVariants: {
-      intent: 'default',
-      size: 'default',
-      withIcon: false,
+    size: {
+      small: 'text-[12px] px-2 py-2',
+      default: 'text-sm px-3 py-2',
+    },
+    withIcon: {
+      true: 'gap-2 items-center',
+      false: '',
     },
   },
-)
+  defaultVariants: {
+    intent: 'default',
+    size: 'default',
+    withIcon: false,
+  },
+})
 
 interface Props
   extends React.ComponentProps<'button'>,
