309cc04b4ce202777f5058acd0f3ad2613bde102
Marco <serikbaev03beka@gmail.com>
2025-04-17 11:02:40 +0500

    refactor(types): add AnimeBaseModel type

--- src/types/anime.types.ts
+++ src/types/anime.types.ts
@@ -1,28 +1,25 @@
 type Tags = string[]
 
-export interface HeroTitle {
+export interface AnimeBaseModel {
   id: number
   slug: string
   title: string
+}
+
+export interface HeroTitle extends AnimeBaseModel {
   subtitle: string
   description: string
   img: string
 }
 
-export interface OngoingTitle {
-  id: number
-  slug: string
-  title: string
+export interface OngoingTitle extends AnimeBaseModel {
   img: string
   tags: Tags
   rating: number
   description: string
 }
 
-export interface RecentVoiceover {
-  id: number
-  slug: string
-  title: string
+export interface RecentVoiceover extends AnimeBaseModel {
   description: string
   img: string
   createdAt: string
