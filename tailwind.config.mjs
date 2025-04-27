f2cd449effb6d6a493c682fd1991db40afd18505
Marco <serikbaev03beka@gmail.com>
2025-04-27 23:28:45 +0500

    chore(deps): add vidstack. hls packages

---
+++ tailwind.config.mjs
@@ -0,0 +1,27 @@
+module.exports = {
+  future: {
+    hoverOnlyWhenSupported: true,
+  },
+  theme: {
+    extend: {
+      colors: {
+        'media-brand': 'rgb(var(--media-brand) / <alpha-value>)',
+        'media-focus': 'rgb(var(--media-focus) / <alpha-value>)',
+      },
+    },
+  },
+  plugins: [
+    require('@vidstack/react/tailwind.cjs')({
+      prefix: 'media',
+    }),
+    customVariants,
+  ],
+}
+
+function customVariants({ addVariant, matchVariant }) {
+  // Strict version of `.group` to help with nesting.
+  matchVariant('parent-data', (value) => `.parent[data-${value}] > &`)
+
+  addVariant('hocus', ['&:hover', '&:focus-visible'])
+  addVariant('group-hocus', ['.group:hover &', '.group:focus-visible &'])
+}
