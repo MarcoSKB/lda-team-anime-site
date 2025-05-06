e0ab01f24567e76e532b5f16e2eda4a5c61eaa6d
Marco <serikbaev03beka@gmail.com>
2025-05-06 19:32:33 +0500

    feat(utils): add readingTimesWithLocale function

--- src/utils/string.ts
+++ src/utils/string.ts
@@ -1,3 +1,5 @@
+import readingTime from 'reading-time'
+
 export const truncateText = (
   text: string,
   maxLength: number = 10,
@@ -6,4 +8,8 @@ export const truncateText = (
   if (withEllipsis && !(text.length <= maxLength))
     return text.substring(-1, maxLength).trimEnd() + '...'
   else return text.substring(-1, maxLength)
+}
+
+export const readingTimesWithLocale = (text: string) => {
+  return Math.ceil(readingTime(text).minutes) + ' минут чтения'
 }
