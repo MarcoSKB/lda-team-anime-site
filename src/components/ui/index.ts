77af0102d19c666820692e83cacce869c06bb7d4
Marco <serikbaev03beka@gmail.com>
2025-05-27 12:44:50 +0500

    feat(ui): add radix ui components

--- src/components/ui/index.ts
+++ src/components/ui/index.ts
@@ -8,4 +8,112 @@ export { default as ReadMore } from './read-more/ReadMore'
 export { default as EpisodeCard } from './episode-card/EpisodeCard'
 export { default as Input } from './input/Input'
 export { default as Switch } from './switch/Switch'
-export { default as Toaster } from './Toaster/Toaster'
+export { default as Toaster } from './toaster/Toaster'
+export { Avatar, AvatarImage, AvatarFallback } from './avatar/Avatar'
+export { Badge, badgeVariants } from './badge/Badge'
+export {
+  Card,
+  CardHeader,
+  CardFooter,
+  CardTitle,
+  CardAction,
+  CardDescription,
+  CardContent,
+} from './card/Card'
+export {
+  type ChartConfig,
+  ChartContainer,
+  ChartTooltip,
+  ChartTooltipContent,
+  ChartLegend,
+  ChartLegendContent,
+  ChartStyle,
+} from './chart/Chart'
+export { Checkbox } from './checkbox/Checkbox'
+export {
+  DropdownMenu,
+  DropdownMenuPortal,
+  DropdownMenuTrigger,
+  DropdownMenuContent,
+  DropdownMenuGroup,
+  DropdownMenuLabel,
+  DropdownMenuItem,
+  DropdownMenuCheckboxItem,
+  DropdownMenuRadioGroup,
+  DropdownMenuRadioItem,
+  DropdownMenuSeparator,
+  DropdownMenuShortcut,
+  DropdownMenuSub,
+  DropdownMenuSubTrigger,
+  DropdownMenuSubContent,
+} from './dropdown-menu/DropdownMenu'
+export { Label } from './label/Label'
+export {
+  Select,
+  SelectContent,
+  SelectGroup,
+  SelectItem,
+  SelectLabel,
+  SelectScrollDownButton,
+  SelectScrollUpButton,
+  SelectSeparator,
+  SelectTrigger,
+  SelectValue,
+} from './select/Select'
+export { Separator } from './separator/Separator'
+export {
+  Sheet,
+  SheetTrigger,
+  SheetClose,
+  SheetContent,
+  SheetHeader,
+  SheetFooter,
+  SheetTitle,
+  SheetDescription,
+} from './sheet/Sheet'
+export {
+  Sidebar,
+  SidebarContent,
+  SidebarFooter,
+  SidebarGroup,
+  SidebarGroupAction,
+  SidebarGroupContent,
+  SidebarGroupLabel,
+  SidebarHeader,
+  SidebarInput,
+  SidebarInset,
+  SidebarMenu,
+  SidebarMenuAction,
+  SidebarMenuBadge,
+  SidebarMenuButton,
+  SidebarMenuItem,
+  SidebarMenuSkeleton,
+  SidebarMenuSub,
+  SidebarMenuSubButton,
+  SidebarMenuSubItem,
+  SidebarProvider,
+  SidebarRail,
+  SidebarSeparator,
+  SidebarTrigger,
+  useSidebar,
+} from './sidebar/Sidebar'
+export { Skeleton } from './skeleton/Skeleton'
+export {
+  Table,
+  TableHeader,
+  TableBody,
+  TableFooter,
+  TableHead,
+  TableRow,
+  TableCell,
+  TableCaption,
+} from './table/Table'
+export { Tabs, TabsList, TabsTrigger, TabsContent } from './tabs/Tabs'
+export { Toggle, toggleVariants } from './toggle/Toggle'
+export { ToggleGroup, ToggleGroupItem } from './toggle-group/ToggleGroup'
+export {
+  Tooltip,
+  TooltipTrigger,
+  TooltipContent,
+  TooltipProvider,
+} from './tooltip/Tooltip'
