dd122758a0fe308cd61e829635cbbe39cc010d68
Marco <serikbaev03beka@gmail.com>
2025-08-18 02:23:05 +0500

    refactor(nav): remove NavSecondary component and update exports

--- src/app/(private)/dashboard/_module/index.tsx
+++ src/app/(private)/dashboard/_module/index.tsx
@@ -1,7 +1,6 @@
 export { AppSidebar } from './app-sidebar/AppSidebar'
 export { SiteHeader } from './site-header/SiteHeader'
 export { NavMain } from './nav-main/NavMain'
-export { NavSecondary } from './nav-secondary/NavSecondary'
 export { NavUser } from './nav-user/NavUser'
 export { SectionCards } from './section-cards/SectionCards'
 export { ChartAreaInteractive } from './chart-area-interactive/ChartAreaInteractive'
