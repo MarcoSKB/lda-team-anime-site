5da7b5fd248c3f4d36350b4d49c0d3e870cb5c95
Marco <serikbaev03beka@gmail.com>
2025-09-22 20:20:48 +0500

    feat(auth): update user roles handling and improve error messages during authentication

--- src/utils/auth.ts
+++ src/utils/auth.ts
@@ -28,7 +28,7 @@ export const { handlers, signIn, signOut, auth } = NextAuth({
           id: profile.sub,
           username: profile.name || profile.email.split('@')[0],
           userAvatar: profile.picture,
-          permission: profile.permission ?? 'user',
+          roles: profile.permission ?? ['User'],
         }
       },
     }),
@@ -42,7 +42,7 @@ export const { handlers, signIn, signOut, auth } = NextAuth({
           username: profile.username,
           email: profile.email,
           userAvatar: `https://cdn.discordapp.com/avatars/${profile.id}/${profile.avatar}.jpeg`,
-          permission: profile.permission ?? 'user',
+          roles: profile.permission ?? ['User'],
         }
       },
     }),
@@ -52,23 +52,37 @@ export const { handlers, signIn, signOut, auth } = NextAuth({
       authorize: async (credentials) => {
         try {
           await signInSchema.validate(credentials)
-          // const res = await fetch(
-          //   `${process.env.NEXT_PUBLIC_API_BASE_URL}/auth/login`,
-          //   {
-          //     method: 'POST',
-          //     headers: { 'Content-Type': 'application/json' },
-          //     body: JSON.stringify(credentials),
-          //   },
-          // )
-          // if (!res.ok) return null
+          const res = await fetch(
+            `${process.env.NEXT_PUBLIC_API_BASE_URL}/auth/login`,
+            {
+              method: 'POST',
+              headers: { 'Content-Type': 'application/json' },
+              body: JSON.stringify(credentials),
+            },
+          )
+          if (!res.ok) {
+            if (res.status === 400 || res.status === 404) {
+              throw new Error('Неверный логин или пароль')
+            }
+            throw new Error('Произошла какая-то ошибка во время авторизации')
+          }
 
-          // const user = await res.json()
+          const {
+            username,
+            email,
+            roles,
+            refreshToken,
+            token: accessToken,
+            expires: expAt,
+          } = await res.json()
+
           return {
-            id: 'ID',
-            username: 'Marco',
-            email: 'Marco@marco.com',
-            permission: 'admin',
-            accessToken: 'fwefwe',
+            username,
+            email,
+            roles,
+            accessToken,
+            refreshToken,
+            expAt,
           }
         } catch (err) {
           if (err instanceof Error) {
@@ -84,23 +98,26 @@ export const { handlers, signIn, signOut, auth } = NextAuth({
       authorize: async (credentials) => {
         try {
           await registerSchema.validate(credentials)
-          // const res = await fetch(
-          //   `${process.env.NEXT_PUBLIC_API_BASE_URL}/auth/register`,
-          //   {
-          //     method: 'POST',
-          //     headers: { 'Content-Type': 'application/json' },
-          //     body: JSON.stringify(credentials),
-          //   },
-          // )
-          // if (!res.ok) throw new Error("Произошла какая-то ошибка во время регистрации")
+          const res = await fetch(
+            `${process.env.NEXT_PUBLIC_API_BASE_URL}/auth/register`,
+            {
+              method: 'POST',
+              headers: { 'Content-Type': 'application/json' },
+              body: JSON.stringify(credentials),
+            },
+          )
+          if (!res.ok)
+            throw new Error('Произошла какая-то ошибка во время регистрации')
 
-          // const user = await res.json()
+          const { refreshToken, accessToken, expires: expAt } = await res.json()
+
           return {
-            id: 'ID',
-            username: 'Marco',
-            email: 'Marco@marco.com',
-            permission: 'user',
-            accessToken: 'fwefwe',
+            username: credentials.nickname as string,
+            email: credentials.email as string,
+            roles: ['User'],
+            expAt,
+            accessToken,
+            refreshToken,
           }
         } catch (err) {
           if (err instanceof Error) {
@@ -112,52 +129,85 @@ export const { handlers, signIn, signOut, auth } = NextAuth({
     }),
   ],
   callbacks: {
-    // async signIn({ user, account, profile }) {
-    //   if (
-    //     account?.provider !== 'credentials' &&
-    //     account?.provider !== 'login' &&
-    //     account?.provider !== 'register'
-    //   ) {
-    //     const userData = {
-    //       provider: account?.provider,
-    //       providerId: account?.providerAccountId,
-    //       username: profile?.name,
-    //       email: profile?.email ?? user.email,
-    //       permission: user.permission ?? 'user',
-    //       userAvatar: user.userAvatar ?? profile?.picture,
-    //     }
+    async signIn({ user, account, profile }) {
+      if (
+        account?.provider !== 'credentials' &&
+        account?.provider !== 'login' &&
+        account?.provider !== 'register'
+      ) {
+        const userData = {
+          provider: account?.provider,
+          providerId: account?.providerAccountId,
+          username: profile?.name,
+          email: profile?.email ?? user.email,
+          roles: user.roles ?? ['user'],
+          userAvatar: user.userAvatar ?? profile?.picture,
+        }
 
-    //     const res = await fetch(
-    //       `${process.env.NEXT_PUBLIC_API_BASE_URL}/auth/oauth`,
-    //       {
-    //         method: 'POST',
-    //         headers: { 'Content-Type': 'application/json' },
-    //         body: JSON.stringify(userData),
-    //       },
-    //     )
+        const res = await fetch(
+          `${process.env.NEXT_PUBLIC_API_BASE_URL}/auth/oauth`,
+          {
+            method: 'POST',
+            headers: { 'Content-Type': 'application/json' },
+            body: JSON.stringify(userData),
+          },
+        )
 
-    //     if (!res.ok) {
-    //       console.error('Ошибка создания пользователя на бэкенде')
-    //       return false
-    //     }
-    //   }
+        if (!res.ok) {
+          console.error('Ошибка создания пользователя на бэкенде')
+          return false
+        }
+      }
 
-    //   return true
-    // },
+      return true
+    },
     async jwt({ token, user }) {
       if (user) {
         token.accessToken = user.accessToken
-        token.permission = user.permission
+        token.refreshToken = user.refreshToken
+        token.roles = user.roles
         token.username = user.username
         token.userAvatar = user.userAvatar
+        token.expAt = user.expAt
       }
-      return token
+
+      if (Date.now() < new Date(token.expAt).getTime()) {
+        return token
+      }
+
+      try {
+        const res = await fetch(
+          `${process.env.NEXT_PUBLIC_API_BASE_URL}/auth/refresh-token`,
+          {
+            method: 'POST',
+            headers: { 'Content-Type': 'application/json' },
+            body: JSON.stringify({
+              refreshToken: token.refreshToken,
+            }),
+          },
+        )
+        const data = await res.json()
+        if (!res.ok) {
+          throw data
+        }
+        return {
+          ...token,
+          accessToken: data.accessToken,
+          refreshToken: data.refreshToken,
+        }
+        // eslint-disable-next-line @typescript-eslint/no-unused-vars
+      } catch (err) {
+        return { ...token, error: 'RefreshTokenError' }
+      }
     },
     async session({ session, token }) {
       session.accessToken = token.accessToken
-      session.user.permission = token.permission
+      session.refreshToken = token.refreshToken
+      session.expAt = token.expAt
+      session.user.roles = token.roles
       session.user.username = token.username
       session.user.userAvatar = token.userAvatar
+      session.error = token.error
       return session
     },
     async redirect({ url, baseUrl }) {
