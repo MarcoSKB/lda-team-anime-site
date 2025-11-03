7e52670d17eb6eb444cee382e34158ba322effcf
Marco <serikbaev03beka@gmail.com>
2025-11-03 21:31:23 +0500

    perf(page): removed unnessesary page

--- src/app/(public)/schedule/_module/index.ts
+++
@@ -1,2 +0,0 @@
-export { default as WeekGroup } from './week-group/WeekGroup'
-export { default as WeekGroupSkeleton } from './week-group/WeekGroupSkeleton'
--- src/app/(public)/schedule/_module/week-group/components/index.ts
+++
@@ -1,2 +0,0 @@
-export { default as WeekItem } from './WeekItem'
-export { default as WeekList } from './WeekList'
